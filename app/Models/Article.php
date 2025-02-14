0accabee25be92e86c8bb89917288c8839e5d7b5
Rohail Shafi <rohailahmad2001@gmail.com>
2025-02-14 18:23:35 +0500

    spatie-laravel-media-library

---
+++ app/Models/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Article extends Model implements HasMedia
+{
+
+    use HasFactory , InteractsWithMedia, SoftDeletes;
+
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config('database.models.' . class_basename(__CLASS__) . '.table');
+        $this->fillable = config('database.models.' . class_basename(__CLASS__) . '.fillable');
+        $this->hidden = config('database.models.' . class_basename(__CLASS__) . '.hidden');
+
+        parent::__construct($attributes);
+    }
+
+}
